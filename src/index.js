7f1a81811b1dc72b1cb7be2f969a113a7915cf3f
Dejapong <dejapong@gmail.com>
2018-09-14 15:03:14 +0530

    Refactoring start for component based organization

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import App from './scenes';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-// import index from './js/index';
-// import React from 'react';
-// import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
-// import store from './store/index';
-// import App from './components/App';
-
-// render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// );
